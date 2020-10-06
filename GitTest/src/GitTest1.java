3e98001ccc87ed1d9c2e0a39814bb6b1b03c71ce
shwang <Dynaudio@DESKTOP-V7U3G1P>
2020-10-06 18:10:33 +0900

    test2 수정

--- GitTest/src/GitTest1.java
+++ GitTest/src/GitTest1.java
@@ -3,6 +3,7 @@ public class GitTest1 {
 
 	public static void main(String[] args) {
 		System.out.println("git test");
+		System.out.println("dd");
 	}
 
 }
--- day07/src/day07/test2.java
+++ day07/src/day07/test2.java
@@ -4,6 +4,7 @@ public class test2 {
 
 	public static void main(String[] args) {
 		System.out.println("????");
+		System.out.println("");
 	}
 
 }
