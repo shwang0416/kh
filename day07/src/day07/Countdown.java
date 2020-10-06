4dca2d393afce9be0e1d53fd14aaef350afa0175
shwang <Dynaudio@DESKTOP-V7U3G1P>
2020-10-06 20:54:47 +0900

    카운트다운

--- day07/src/day07/Countdown.java
+++ day07/src/day07/Countdown.java
@@ -3,7 +3,7 @@ package day07;
 import java.util.*;
 public class Countdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("숫자를 입력하세요 :");
@@ -11,6 +11,7 @@ public class Countdown {
 		
 		for(;num >= 0;num--) {
 			System.out.println(num);
+			Thread.sleep(1000L); //1000 밀리초 == 1초 휴식
 		}
 		
 	}
