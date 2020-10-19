e327bb1b3506b2a3bd5ea84285bd1d1c5a717aaf
shwang <Dynaudio@DESKTOP-V7U3G1P>
2020-10-19 18:16:23 +0900

    day15 테스트

---
+++ day15/src/oop/inherit6/Test01.java
@@ -0,0 +1,12 @@
+package oop.inherit6;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//추상클래스는 인스턴스 생성이 불가능
+		//File f = new File("test.txt");
+		//Cannot instantiate the type File
+		
+	}
+
+}
