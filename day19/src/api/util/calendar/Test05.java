c7d052209c7349b5d93d2cd25bc2a1499e9d0e8b
shwang <Dynaudio@DESKTOP-V7U3G1P>
2020-10-26 16:24:53 +0900

    달력 출력(Calendar & Date 2)

--- day19/src/api/util/calendar/Test05.java
+++ day19/src/api/util/calendar/Test05.java
@@ -9,7 +9,6 @@ public class Test05 {
 /*
  * 사용자에게 연도와 월을 입력받아서 해당하는 달의 달력 출력해주세요.
  * - 달력은 무조건 6줄로 나와야 하며, 전 달과 다음 달이 나올 수 있습니다.
- * 이 문제를 풀려면 더하고 빼고 지지고 볶아야함,,,,,,
  * - 한 줄에 7개씩 탭 키로 간격을 벌려 출력
  * - 7개를 출력하면 개행을 해서 다음 줄에 다시 출력
  * - 힌트 : 요일을 계산해야 하는데, 요일에 해당하는 상수는 Calendar.DAY_OF_WEEK입니다
@@ -33,13 +32,14 @@ public class Test05 {
 		month = scan.nextInt();
 		c.set(year,month - 1,1); //month를 set할 때는 -1
 		
+		int week = Calendar.DAY_OF_WEEK;
+		//일요일부터 세었을 때 week번째 요일이다. (
 		
-		while(Calendar.DAY_OF_WEEK + from != Calendar.WEDNESDAY)
-			from--;
-		c.add(Calendar.DATE, from);	
+		c.add(Calendar.DATE, -(week - 1));
+		//week-1만큼 앞으로 돌아감
 		//달력표기 시작날짜 지정
 		
-		SimpleDateFormat f= new SimpleDateFormat("E	");
+		SimpleDateFormat f = new SimpleDateFormat("E	");
 		Date d;
 		for(int i = 0; i < 7; i++) {
 			d = c.getTime();
