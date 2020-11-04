a7b96644b708fdb4543d17f2c5ca92fdd93ec378
shwang <Dynaudio@DESKTOP-V7U3G1P>
2020-11-04 16:11:30 +0900

    FlowLayout

---
+++ day27/src/gui/Test03_1.java
@@ -0,0 +1,44 @@
+package gui;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Test03_1 {
+	public static void main(String[] args) {
+		
+		//Frame 생성 코드 복붙 
+		JFrame frame = new JFrame();//창 생성
+		frame.setTitle("자바 그래픽 예제");
+		frame.setLocation(300, 100);//왼쪽 위 모서리 위치(x, y) - px
+		frame.setSize(400, 400);//크기(폭,높이) - px(pixel,화소)
+		frame.setResizable(false);//창 크기변경 불가
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//x를 누르면 프로그램이 종료되도록 설정
+		
+		//GridLayout을 이용하여 컴포넌트를 배치하는 방법
+//		FlowLayout layout =new FlowLayout(); //정렬방향,가로갭,세로갭
+		FlowLayout layout =new FlowLayout(FlowLayout.CENTER); //정렬방향,가로갭,세로갭
+//		FlowLayout(조정, 가로갭, 세로갭);
+		frame.setLayout(layout);
+		
+		//set Component
+		JButton a =new JButton("HI");
+		JButton b =new JButton("Click here");
+		JButton c =new JButton("Drink me");
+		JButton d =new JButton("Lol");
+		
+		frame.add(a);
+		frame.add(b);
+		frame.add(c);
+		frame.add(d);
+		//frame.add(b); 각각의 컴포넌트들은 한번씩만 추가가 가능
+//		: List보다는 Set과 비슷한 성질이라고 할 수 있음
+		//지뢰찾기, 계산기 만들 때 활용가능
+		
+
+
+		frame.setVisible(true);//창을 보이게 하는 명령
+	}
+}
