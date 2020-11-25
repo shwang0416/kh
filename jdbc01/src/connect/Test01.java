2c7a8670b23fc16a4cecd557ff8d40877197de7d
shwang0416 <crystalhwang0416@gmail.com>
2020-11-25 14:26:45 +0900

    JDBC 개념

---
+++ jdbc01/src/connect/Test01.java
@@ -0,0 +1,14 @@
+package connect;
+
+public class Test01 {
+	public static void main(String[] args) {
+		/*
+		 * JDBC(java DataBase Connectivity)
+		 * Java에서 Database에 연결하기 위한 모든 형태의 작업
+		 * 
+		 * => Java에 'Oracle'에 연결할 것임을 알려줘야 함.
+		 * => Oracle에서 제공하는 자바를 위한 라이브러리를 등록해야 함.
+		 * 
+		 */
+	}
+}
